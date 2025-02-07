58093d282e53e0217166fda79ec0e202b2707061
Maze711 <badlonmazeclarion@gmail.com>
2025-02-07 17:48:31 +0800

    chore: Improve login error handling and validation

--- backend/controller/login_controller.py
+++ backend/controller/login_controller.py
@@ -9,9 +9,15 @@ auth_bp = Blueprint("auth", __name__)
 def login():
     data = request.json
     alumni_id = data.get("alumni_id")
-    password = data.get("password")
+    password = data.get("password")    
+
+    if not alumni_id or not password:
+        return jsonify({"error": "Missing alumni_id or password"}), 400
 
     user = Config.fetch_alumni_details(alumni_id)
+    
+    if user is None:
+        return jsonify({"error": "Invalid alumni_id"}), 401
 
     # if user and check_password_hash(user["password"], password): use this when doing a hashing
     if user and user["password"] == password:
@@ -26,5 +32,7 @@ def login():
             "full_name": full_name,
             "role" : user["role"]
         })
+    elif password != user['password']:
+        return jsonify({"error": "Wrong Password"}), 401
     else:
         return jsonify({"error": "Invalid credentials"}), 401
